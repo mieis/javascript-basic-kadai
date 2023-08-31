3272900017342dbd9c799e597d5bc0c371d74c5c
mieis <143199878+mieis@users.noreply.github.com>
2023-08-31 23:39:35 +0900

    Add files via upload

---
+++ kadai_009/holiday.js
@@ -0,0 +1,11 @@
+const onigiriLanking = ["ツナマヨ", "明太子", "梅干し", "いくら", "えびマヨ", "焼きたらこ", "昆布", "鮭ハラミ", "塩", "とり五目", "赤飯", "天むす", "チャーハン"];
+
+for (let i = 0; i <= 13; i++) {
+    console.log(onigiriLanking[i]);
+}
+
+let n = 0;
+while (n < 14) {
+    console.log(onigiriLanking[n]);
+    n++;
+}
