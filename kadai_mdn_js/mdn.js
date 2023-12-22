4ec6b6568c2254640aafcefd1162de55d38f2cea
mieis <143199878+mieis@users.noreply.github.com>
2023-12-22 10:41:31 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,9 @@
 const date = new Date();
 const [year, month, day] = [
     date.getFullYear(),
-    date.getMonth(),
+    date.getMonth() + 1,
     date.getDate(),
 ];
 
+console.log(date);
 console.log(year + '年' + month + '月' + day + '日');
